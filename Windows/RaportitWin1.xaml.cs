48fe8a3281d70e15a494438d62c156e3e4b901cf
Patrick Varkoi <patrick.varkoi@edu.karelia.fi>
2026-03-25 03:08:41 +0200

    Redesign invoice reporting UI and add detail view
    Major overhaul of invoice (lasku) reporting windows:
    - RaportitWin1 now displays invoices in columns with key info.
    - Added "Open" button to view invoice details in RaportitWin2.
    - RaportitWin2 redesigned with grouped sections for invoice, customer, reservation, services, and summary.
    - Added print and navigation actions to detail view.
    - Refactored data binding to use Lasku objects and repository.
    - Improved error handling and removed obsolete code.
    - UI is now more user-friendly and supports printing.

--- Windows/RaportitWin1.xaml.cs
+++ Windows/RaportitWin1.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Vuokratoimisto_projekti.Classes;
 using Vuokratoimisto_projekti.Repositories;
 
@@ -22,66 +11,44 @@ namespace Vuokratoimisto_projekti.Windows
     /// </summary>
     public partial class RaportitWin1 : Window
     {
-        private ObservableCollection<string> osittaisetLaskut;
         private readonly LaskuTiedotRepo repo;
-        private Lasku selectedLasku;
-        private Raportti Raportti;
+
         public RaportitWin1()
         {
             InitializeComponent();
-            /*
+            
             repo = new LaskuTiedotRepo();
-            repo.HaeOsittaisetLaskut();*/
-
-            Raportti = new Raportti();
-            DataContext = this;
-
-            // Example data
-            Raportti.Add(new Palvelu { Nimi = "Palvelu1" });
-            Raportti.Add(new Huone { Nimi = "Huone1" });
-
-            ObservableCollection<string> osittaisetLaskut = new ObservableCollection<string> { Raportti.ToString() };
-            LaskutListBox.ItemsSource = osittaisetLaskut;
-
-
+            DataContext = repo;
+            
+            // Hae laskut tietokannasta
+            repo.HaeOsittaisetLaskut();
         }
-        public ObservableCollection<string> OsittaisetLaskut
-        {
-            get { return osittaisetLaskut; }
-        }
-
 
         private void RaporttiListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            // Event handler for future use
         }
 
         private void AvaaBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (LaskutListBox.SelectedItem != null)
+            if (LaskutListBox.SelectedItem is Lasku selectedLasku)
             {
-                Lasku selectedLasku = LaskutListBox.SelectedItem as Lasku;
+                // Hae täydelliset tiedot tietokannasta
+                Lasku kokoLasku = repo.GetKokoLasku(selectedLasku.ID);
 
-                if (selectedLasku != null)
+                if (kokoLasku != null)
                 {
-                    // Hae täydelliset tiedot tietokannasta
-                    Lasku kokoLasku = repo.GetKokoLasku(selectedLasku.ID);
-
-                    if (kokoLasku != null)
-                    {
-                        RaportitWin2 raportitWin2 = new RaportitWin2(kokoLasku);
-                        raportitWin2.Show();                        
-                    }
-                    else
-                    {
-                        MessageBox.Show("Tietoja ei löytynyt.");
-                    }
+                    RaportitWin2 raportitWin2 = new RaportitWin2(kokoLasku);
+                    raportitWin2.Show();
                 }
-
+                else
+                {
+                    MessageBox.Show("Tietoja ei löytynyt.");
+                }
             }
             else
             {
-                MessageBox.Show("Valitse ensin asiakas listalta.");
+                MessageBox.Show("Valitse ensin lasku listalta.");
             }
         }
 
--- Windows/RaportitWin2.xaml.cs
+++ Windows/RaportitWin2.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Vuokratoimisto_projekti.Classes;
 
 namespace Vuokratoimisto_projekti.Windows
 {
@@ -19,9 +10,41 @@ namespace Vuokratoimisto_projekti.Windows
     /// </summary>
     public partial class RaportitWin2 : Window
     {
-        public RaportitWin2(Classes.Lasku kokoLasku)
+        private Lasku lasku;
+
+        public RaportitWin2(Lasku kokoLasku)
         {
             InitializeComponent();
+            
+            if (kokoLasku == null)
+            {
+                MessageBox.Show("Laskun tiedot ovat puutteelliset.");
+                this.Close();
+                return;
+            }
+
+            lasku = kokoLasku;
+            DataContext = lasku;
+        }
+
+        private void TakaisinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TulostaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintVisual(this, "Lasku");
+            }
+        }
+
+        private void VieAsiakasTietoihinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Avaa asiakastietojen muokkaus-ikkuna
+            MessageBox.Show($"Asiakkaan {lasku.AsiakasNimi} tietojen avaaminen.");
         }
     }
 }
